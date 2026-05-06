aba1eb55a09a1f13dd3a0a505df318a5cb4365d8
Jie Zhang <zhanj@microsoft.com>
2026-05-06 04:15:08 +0000

    Add version.h
    Signed-off-by: Jie Zhang <zhanj@microsoft.com>

---
+++ include/version.h
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#ifndef MRC_NCCL_PLUGIN_VERSION_H
+#define MRC_NCCL_PLUGIN_VERSION_H
+
+#define MRC_NCCL_PLUGIN_VERSION_MAJOR 0
+#define MRC_NCCL_PLUGIN_VERSION_MINOR 1
+#define MRC_NCCL_PLUGIN_VERSION_PATCH 0
+
+
+#endif /* MRC_NCCL_PLUGIN_VERSION_H */
